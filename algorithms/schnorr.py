79a1e7677a9b7581824398b14be5740e5f838438
Jonas Skjodt <skjo@itu.dk>
2025-09-02 10:52:33 +0200

    algorithm set up
    Co-authored-by: Alex-itu <lolk@itu.dk>

---
+++ algorithms/schnorr.py
@@ -0,0 +1,24 @@
+# imports
+
+
+# key generation
+
+#### private key
+
+#### public key
+
+
+
+
+# signing
+
+#### message 
+# we use pythons b to make the string a bytes object (not a regular unicode string)
+# so in b'hey' is the ascii values for h,e,y
+msg = b'hey'
+
+## hashing
+
+
+
+# verification
