747844046edd143bee289e7ed1293333e364dacd
Jonas Skjodt <skjo@itu.dk>
2025-09-17 11:40:24 +0200

    setup continued, utils integrate

---
+++ src/utils/elgamal_params.py
@@ -0,0 +1,29 @@
+# Parameters generation for ElGamal
+# PyCryptodome 
+from Crypto.Util import number
+import random
+
+def prime_p():
+    return number.getPrime(random.randint(2000, 2024))
+
+def generator_g():
+    return 35 # add elgamlCore here. get the highest g, not how it should be made, but for testing it is okay
+
+def secret_key():
+    return random.randint(2, (prime_p()-1))
+
+def A(secretKey_a, largePrime_p, generatorOfGroup_g):
+    return pow(generatorOfGroup_g, secretKey_a, largePrime_p)
+
+def public_key(largePrime_p, generatorOfGroup_g, A):
+    return (largePrime_p, generatorOfGroup_g, A)
+
+def el_setup():
+    largePrime_p = prime_p()
+    generatorOfGroup_g = generator_g()
+    secretKey_a = secret_key()
+    A_value = A(secretKey_a, largePrime_p, generatorOfGroup_g)
+    pk = public_key(largePrime_p, generatorOfGroup_g, A_value)
+    return pk, secretKey_a
+
+# print(el_setup())
