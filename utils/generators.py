2f94f4ccee15faa0a62598b53b682f3b35fa7461
Jonas Skjodt <skjo@itu.dk>
2025-09-25 13:42:11 +0200

    cleanup and tests
    Co-authored-by: Alex-itu <lolk@itu.dk>

--- utils/generators.py
+++ utils/generators.py
@@ -11,11 +11,9 @@
 # TODO
 # integrate non-interactive proof of zero knowledge (NIZKPK) for the secret key sk corresponding to pk
 
-
 from petlib.ec import EcGroup
 import random
 import utils.signature as Sig
-import utils.NIZKP as NIZKP
 
 def pub_param(nid=713):
     group_G = EcGroup()
@@ -30,26 +28,3 @@ def skey_gen(id=random, pp=None):
         pp = pub_param()
     sk, pk = Sig.key_gen(pp)
     return ((id, pk), sk)
-
-
-# test with NIZPKP
-def test():
-    params = pub_param()
-    sk, pk = Sig.key_gen(params)
-
-    # Schnorr signature
-    msg = "hello signature"
-    signature = Sig.schnorr_sign(params, sk, msg)
-    print("Signature (R, s):", signature)
-    is_valid = Sig.schnorr_verify(params, pk, msg, signature)
-    print("Signature valid?", is_valid)
-
-    # Schnorr NIZKP
-    msg_zkp = "hello proof"
-    proof = NIZKP.schnorr_proof(params, pk, sk, msg_zkp)
-    print("Proof (c, r, W):", proof)
-    proof_valid = NIZKP.schnorr_NIZKP_verify(params, pk, proof, msg_zkp)
-    print("Proof valid?", proof_valid)
-
-if __name__ == "__main__":
-    test()
