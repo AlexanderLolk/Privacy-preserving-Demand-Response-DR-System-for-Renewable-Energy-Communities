51543febdebfb1f47a01f89d7e190de661ed236f
Jonas Skjodt <skjo@itu.dk>
2025-12-02 12:53:47 +0100

    elgamal: threshold, partial decrypt add
    Co-authored-by: Alex-itu <lolk@itu.dk>

--- utils/ec_elgamal.py
+++ utils/ec_elgamal.py
@@ -9,165 +9,144 @@ from Crypto.PublicKey import ECC
 # pk                = Q
 # sk                = d
 
+class ElGamal:
 
-def test_tp_decrypt():
-    curve_params = tc.CurveParameters('P-256')
-    print("curve_params: " + str(curve_params))
-    print("order: " + str(type(curve_params.order)))
-    
-    # key = ECC.generate(curve="P-256")
-    # print("key from ECC: " + str(key))
-    
-
-    thresh_params = tc.ThresholdParameters(2, 2)
-
-    pub_key, key_shares = tc.create_public_key_and_shares_centralized(curve_params, thresh_params)
-
-    # print("len of key_shares: " + str(len(key_shares)))
-    encrypted_message = tc.encrypt_message(str(123), pub_key)
-
-    reconstruct_shares = [key_shares[0],key_shares[1]]  # choose 3 of 5 key shares
-
-    partial_decryptions = [tc.compute_partial_decryption(encrypted_message, share) for share in reconstruct_shares]
-
-    decrypted_message = tc.decrypt_message(partial_decryptions, encrypted_message, thresh_params)
-
-    print(decrypted_message)
-   
-# test_tp_decrypt()
-
-def test_tp_deterministic_decrypt():
-    curve_params = tc.CurveParameters('P-256')
-    print("order: " + str(curve_params.order))
-    print("order type: " + str(type(curve_params.order)))
-    
-    thresh_params = tc.ThresholdParameters(2, 2)
-    pub_key, key_shares = tc.create_public_key_and_shares_centralized(curve_params, thresh_params)
-    encrypted_message = tc.encrypt_message(str(0), pub_key)
-    
-    try_encrypted_message
-
-
-# needed for ekey_gen
-def key_gen(sec_params):
-    thresh_params = tc.ThresholdParameters(2, 2)
-    public_key, key_shares = tc.create_public_key_and_shares_centralized(sec_params, thresh_params)
-    return public_key, key_shares
-
-def enc(public_key, pp, message):
-    
-    if not isinstance(message, str):
-        message = str(message)
+    def __init__(self, curve_name="P-256"):
+        self.curve = tc.CurveParameters(curve_name)
         
-    return tc.encrypt_message(message, public_key)  
+    def keygen(self):
+        """
+        """
+        x = tc.number.random_in_range(2, self.curve.order)
+        return x, x * self.curve.P
 
-def part_dec(encrypted_message, key_share):
-    return tc.compute_partial_decryption(encrypted_message, key_share)
+    def encrypt(self, public_key, message):
+        """
+        """
+        r = tc.number.random_in_range(2, self.curve.order)
 
-def dec(partial_decryptions, encrypted_message, threshold_params):
-    return tc.decrypt_message(partial_decryptions, encrypted_message, threshold_params)
+        if isinstance(message, int):
+            message = message * self.curve.P
+            
+        if hasattr(public_key, 'Q'):
+            pk_point = public_key.Q
+        else:
+            pk_point = public_key
 
-
-
-# from petlib.bn import Bn
-# from petlib.ec import EcPt
-
-# def key_gen(params):
-#     """Generates a fresh key pair
-
-#     :param params: 
-
-#     """
-#     _, g, o = params
-#     priv = o.random()
-#     pub = priv * g
-#     return (pub, priv)
-
-# # standard elgamal encryption
-# # def enc(pub, params, counter):
-# #     G, g, o = params
-# #     # k = r
-# #     k = o.random()
-# #     #   r * g 
-# #     a = k * g
-# #     #   r * ek  +    m    * g
-# #     b = k * pub + counter * g
-# #     return (a, b)
-
-# def enc(pub, params, counter):
-#     """
-
-#     :param pub: 
-#     :param params: 
-#     :param counter: 
-
-#     """
-#     G, g, o = params
-#     # k = r
-#     k = o.random()
-#     #   r * g  
-#     # a = k.pt_mul(g) 
-#     a = g.pt_mul(k)
-
-#     # if not isinstance(counter, Bn) and not isinstance(counter, EcPt):
-#     #     counter = Bn(counter)
-#     if isinstance(counter, EcPt):
-#         m_point = counter
-#     elif isinstance(counter, Bn):
-#         m_point = g.pt_mul(counter)
-#     else:
-#         m_point = g.pt_mul(Bn(counter))
+        c1 = r * self.curve.P
+        c2 = (r * pk_point) + message
+        # return [r * self.curve.P, (r * public_key.Q) + (message), r,]
+        return [c1, c2]
     
-#     #   ek * r  +    g   *  m
-#     # b = pub.pt_mul(k).pt_add(g.pt_mul(counter))
-#     b = pub.pt_mul(k).pt_add(m_point)
-#     # b =  counter + pub.pt_mul(k) 
-#     return (a, b)
-
-# def make_table(params):
-#     """Make a decryption table
-
-#     :param params: 
-
-#     """
-#     _, g, o = params
-#     table = {}
-#     for i in range(-1000, 1000):
-#         table[i * g] = i
-#     return table
-
-# def dec(priv, params, table, c1):
-#     """Decrypt an encrypted counter
-
-#     :param priv: 
-#     :param params: 
-#     :param table: 
-#     :param c1: 
-
-#     """
-#     _, g, o = params
-#     a, b = c1
-#     # plain = b + (-priv * a)
-#     plain = b.pt_add(a.pt_mul(-priv))
-#     return table[plain]
+    def really_decrypt_message_for_real(self, message_point):
+        """
+        """
+        for i in range(1000):
+            if (i * self.curve.P) == message_point:
+                return i
+            
+    def decrypt(self, secret_key, ciphertext):
+        """
+        """
+        c1 = ciphertext[0]
+        c2 = ciphertext[1]
+        s = (self.curve.order + -secret_key) * c1
+        return self.really_decrypt_message_for_real(c2 + s)
+           
+    # threshold partial decryption
+    def keygen_threshold(self, threshold=True, t=2, n=2):
+        """
+        """
+        if threshold:
+            thresh_params = tc.ThresholdParameters(t, n)
+            public_key, key_shares = tc.create_public_key_and_shares_centralized(self.curve, thresh_params)
+            return public_key, key_shares
     
+    def partial_threshold_decrypt(self, encrypted_message, key_share):
+        encrypted_message = tc.EncryptedMessage(encrypted_message[0], encrypted_message[0], bytes(1))
+        return tc.compute_partial_decryption(encrypted_message, key_share)
 
+    def decrypt_threshold(self, partial_decryptions, encrypted_message, thres_params):
+        return tc.decrypt_message(partial_decryptions, encrypted_message, thres_params)
     
-
-# def demo(params, ek, dk):
-#     """
-
-#     :param params: 
-#     :param ek: 
-#     :param dk: 
-
-#     """
-#     # msg = Bn.from_binary(dk)
-#     msg = Bn(5)
-#     print("Original message: ", str(msg))
-#     enc_msg = enc(ek, params, msg)
-#     # print("Encrypted message: ", str(enc_msg))
-#     table = make_table(params)
-#     dec_msg = dec(dk, params, table, enc_msg)
-#     print("Decrypted message: ", str(dec_msg))
+    def partial_decrypt(self, ciphertext, key_share: tc.KeyShare):
+        # c0 = ciphertext[0]
+        # v_y = c0 * key_share.y
+        # v_y = ciphertext * key_share.y
+        # return tc.PartialDecryption(key_share.x, v_y, self.curve)
+        c0 = ciphertext[0]
+        v_y = c0 * key_share.y
+        return tc.PartialDecryption(key_share.x, v_y, self.curve)
     
+    def threshold_decrypt(
+        self,
+        partial_decryptions: list,
+        encrypted_message: list,
+        threshold_params: tc.ThresholdParameters,
+    ):
+        """Combines multiple partial decryptions to obtain the original message"""
+        
+        partial_indices = [dec.x for dec in partial_decryptions]
+        lagrange_coefficients = [
+            tc.lagrange_coefficient_for_key_share_indices(
+                partial_indices, idx, self.curve
+            )
+            for idx in partial_indices
+        ]
+
+        summands = [
+            lagrange_coefficients[i].coefficient * partial_decryptions[i].yC1
+            for i in range(0, len(partial_decryptions))
+        ]
+        restored_kdP = tc.number.ecc_sum(summands)
+
+        c2 = encrypted_message[1]  # Extract C2 from ciphertext
+        restored_point = c2 + (-restored_kdP)
+
+        # Convert point back to integer
+        return self.really_decrypt_message_for_real(restored_point)
+
+    def test_hyp_elgamal(self):
+        sk, pk = self.keygen()
+        print("sk is: " + str(sk))
+        print("pk is: " + str(pk))
+        
+        m = 123
+        print("Original message (integer): " + str(m))
+        
+        cipher = self.encrypt(pk, m)
+        print("Encrypted message: " + str(cipher))
+    
+        message_dec_int = self.decrypt(sk, cipher)
+        print("Decrypted message (integer): " + str(message_dec_int))
+        
+        assert m == message_dec_int, f"Expected {m}, got {message_dec_int}"
+
+    def test_threshold_elgamal(self):
+            
+        pub_key, key_shares = self.keygen_threshold(threshold=True, t=2, n=2)
+        print("Generated 2-of-2 threshold keys")
+
+        m = 123
+        print(f"Original message: {m}")
+
+        encrypted_msg = self.encrypt(pub_key, m)
+        print("Encrypted message: " + str(encrypted_msg))
+
+        partial_decs = [
+            self.partial_decrypt(encrypted_msg, key_shares[0]),
+            self.partial_decrypt(encrypted_msg, key_shares[1])
+        ]
+        print("Computed 2 partial decryptions")
+
+        thresh_params = tc.ThresholdParameters(2, 2)
+        # decrypted_msg = self.decrypt_threshold(partial_decs, encrypted_msg, thresh_params)
+        decrypted_msg = self.threshold_decrypt(partial_decs, encrypted_msg, thresh_params)
+        print(f"Decrypted message: {decrypted_msg}")
+
+        # assert str(m) == decrypted_msg, "Decryption failed"
+        assert m == decrypted_msg, f"Decryption failed: expected {m}, got {decrypted_msg}"
+        
+el = ElGamal()
+el.test_hyp_elgamal()
+el.test_threshold_elgamal()
